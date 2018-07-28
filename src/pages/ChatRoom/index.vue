361c835c3b80a6d703fc92cb139a436c1b35f2e0
Zero <silverfantacy@gmail.com>
2018-07-28 15:38:20 +0800

    連接 Firebase

--- src/pages/ChatRoom/index.vue
+++ src/pages/ChatRoom/index.vue
@@ -1,6 +1,82 @@
 <script>
+let chatDataRef = firebase.database().ref('/chatData/')
 export default {
-  name: 'chatRoom'
+  name: 'chatRoom',
+  components: {},
+  data() {
+    return {
+      tempMessage: '',
+      username: '',
+      message: '',
+      messages: [
+        // {
+        //   username: 'Zero',
+        //   message: 'Hello 你好',
+        //   timestamp: '2018/08/04'
+        // },
+        // {
+        //   username: 'Casper',
+        //   message: 'Hello',
+        //   timestamp: '2018/08/04'
+        // }
+      ]
+    }
+  },
+  methods: {
+    submitMessage() {
+      let vm = this
+      // 避免亂數產生，使用時間命名
+      var timestamp = Math.floor(Date.now())
+      // console.log(timestamp)
+
+      // 計算時間
+      function timeStamp2String(time) {
+        var datetime = new Date()
+        datetime.setTime(time)
+        var year = datetime.getFullYear()
+        var month = datetime.getMonth() + 1
+        var date = datetime.getDate()
+        var hour = datetime.getHours()
+        var minute = datetime.getMinutes()
+        var second = datetime.getSeconds()
+        return (
+          year +
+          '-' +
+          month +
+          '-' +
+          date +
+          ' ' +
+          hour +
+          ':' +
+          minute +
+          ':' +
+          second
+        )
+      }
+      var nowDate = timeStamp2String(timestamp)
+      // console.log(nowDate)
+      // 測試是否取得Data
+      // console.log({
+      //   username: vm.username,
+      //   message: vm.tempMessage,
+      //   time: new Date()
+      // })
+      chatDataRef.child(timestamp).set({
+        time: nowDate,
+        username: vm.username,
+        message: vm.tempMessage
+      })
+      vm.tempMessage = ''
+    }
+  },
+  mounted() {
+    let vm = this
+    chatDataRef.orderByChild('timestamp').on('value', function(snapshot) {
+      var val = snapshot.val()
+      // console.log(val)
+      vm.messages = val
+    })
+  }
 }
 </script>
 
