3bc192833cb9166ccc494270899a1342ca06af39
Zero <silverfantacy@gmail.com>
2018-07-28 11:43:34 +0800

    版面概況

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import 'bootstrap'
+import 'jquery'
+import 'popper.js'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/chatroom',
+      name: 'ChatRoom',
+      component: () => import('@/pages/ChatRoom')
+    },
+    {
+      path: '*',
+      redirect: '/chatroom'
     }
   ]
 })
